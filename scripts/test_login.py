aa65ebf590ad15e2ab1f327d42f0b32f95578709
wuchuyi112 <wuchuyi@example.com>
2019-03-10 10:35:21 +0800

    跳转到登录页面

--- scripts/test_login.py
+++ scripts/test_login.py
@@ -1,4 +1,13 @@
+from base.base_driver import init_driver
+import time
+
 class TestLogin:
+    def setup(self):
+        self.driver = init_driver()
+
+    def teardown(self):
+        time.sleep(2)
+        self.driver.quit()
 
     def test_login1(self):
         assert 1
